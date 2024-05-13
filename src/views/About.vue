9bff8c769b7ab1cf2a2d9e75fba889b9baf3d1ea
luisllarena <luisvicente.llarena.cics@ust.edu.ph>
2024-05-13 09:52:21 +0800

    "about"

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,30 @@
 <template>
-    <H1>About us</H1>
+    <div class="about">
+        <H1>About us</H1> 
+    </div>
+    <div class="about">
+      <h3>Welcome to PetPals Paradise, your ultimate destination for all things pets! Founded by Aimee Soriano, Gericho Nolasco, Luis Llarena, Diego Jumagdao, and John Villamor, PetPals is a passion project born out of our love for animals.</h3> 
+    </div>
+    <div class="about">
+      <h3>At PetPals, we believe that every pet deserves a loving home, and we're dedicated to helping you find the perfect furry friend to bring into your life. Whether you're looking for a loyal dog, a playful cat, or any other type of pet, we've got you covered.</h3>
+    </div>
+    <div class="about">
+      
+      <h3>Our team is committed to providing top-quality products and services to ensure the happiness and well-being of both you and your pet. From premium pet food and toys to cozy beds and stylish accessories, we've curated an extensive selection of items to meet all your pet care needs.</h3>
+    </div>
+    <div class="about">
+      <h3>Join us in our mission to make the world a better place for pets and pet owners alike. Explore our store today and discover the joy of pet companionship with PetPals!</h3>
+    </div>
 </template>
+
+<style scoped>
+@media (min-width: 1024px) {
+    .about {
+    min-height: 15vh;
+    display: flex;
+    justify-content: center; 
+    align-items: center; 
+    color: #D04C00;
+  }
+}
+</style>
